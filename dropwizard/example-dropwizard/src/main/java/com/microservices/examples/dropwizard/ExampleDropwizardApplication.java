6c5e856df49d8eda5e89c055a28bf78af06842a5
Ciprian Mihaila <cipri.mihaila@gmail.com>
2018-10-02 21:57:45 +0300

    dropwizard comunicate with other service

--- dropwizard/example-dropwizard/src/main/java/com/microservices/examples/dropwizard/ExampleDropwizardApplication.java
+++ dropwizard/example-dropwizard/src/main/java/com/microservices/examples/dropwizard/ExampleDropwizardApplication.java
@@ -1,8 +1,13 @@
 package com.microservices.examples.dropwizard;
 
+import javax.ws.rs.client.Client;
+
+import com.microservices.examples.dropwizard.resources.GreeterRestResource;
+import com.microservices.examples.dropwizard.resources.GreeterSayingFactory;
 import com.microservices.examples.dropwizard.resources.HolaRestResource;
 
 import io.dropwizard.Application;
+import io.dropwizard.client.JerseyClientBuilder;
 import io.dropwizard.configuration.EnvironmentVariableSubstitutor;
 import io.dropwizard.configuration.SubstitutingSourceProvider;
 import io.dropwizard.setup.Bootstrap;
@@ -28,6 +33,13 @@ public class ExampleDropwizardApplication extends Application<ExampleDropwizardC
 	@Override
 	public void run(final ExampleDropwizardConfiguration configuration, final Environment environment) {
 		environment.jersey().register(new HolaRestResource(configuration.getSayingFactory().getSaying()));
+
+		// greeter service
+		GreeterSayingFactory greeterSayingFactory = configuration.getGreeterSayingFactory();
+		Client greeterClient = new JerseyClientBuilder(environment).using(greeterSayingFactory.getJerseyClientConfig())
+				.build("greeterClient");
+		environment.jersey().register(new GreeterRestResource(greeterSayingFactory.getSaying(),
+				greeterSayingFactory.getHost(), greeterSayingFactory.getPort(), greeterClient));
 	}
 
 }
--- dropwizard/example-dropwizard/src/main/java/com/microservices/examples/dropwizard/ExampleDropwizardConfiguration.java
+++ dropwizard/example-dropwizard/src/main/java/com/microservices/examples/dropwizard/ExampleDropwizardConfiguration.java
@@ -1,6 +1,7 @@
 package com.microservices.examples.dropwizard;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.microservices.examples.dropwizard.resources.GreeterSayingFactory;
 import com.microservices.examples.dropwizard.resources.HelloSayingFactory;
 
 import io.dropwizard.Configuration;
@@ -8,6 +9,7 @@ import io.dropwizard.Configuration;
 public class ExampleDropwizardConfiguration extends Configuration {
 
 	private HelloSayingFactory sayingFactory;
+	private GreeterSayingFactory greeterSayingFactory;
 
 	@JsonProperty("helloapp")
 	public HelloSayingFactory getSayingFactory() {
@@ -17,6 +19,16 @@ public class ExampleDropwizardConfiguration extends Configuration {
 	@JsonProperty("helloapp")
 	public void setSayingFactory(HelloSayingFactory sayingFactory) {
 		this.sayingFactory = sayingFactory;
+	}
+
+	@JsonProperty("greeter")
+	public GreeterSayingFactory getGreeterSayingFactory() {
+		return greeterSayingFactory;
+	}
+
+	@JsonProperty("greeter")
+	public void setGreeterSayingFactory(GreeterSayingFactory greeterSayingFactory) {
+		this.greeterSayingFactory = greeterSayingFactory;
 	}
 
 }
